7a46041731a218fe3f90b205fd00e787bbfe377c
Karol Szcześniak <karolszczesniakk@gmail.com>
2022-05-20 16:34:06 +0200

    change cube material and add light sources

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,13 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 
 document.body.appendChild(renderer.domElement);
-
 const camera = new THREE.PerspectiveCamera(
   70,
   window.innerWidth / window.innerHeight,
   0.01,
   100
 );
-camera.position.y = 0;
-camera.position.z = 2
-
-
+camera.position.z = 2;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.maxDistance = 5;
@@ -29,39 +25,36 @@ const planeGeometry = new THREE.BoxGeometry(10000, 0.1, 10000);
 const skyGeometry = new THREE.BoxGeometry(10000, 0.1, 10000);
 const sunGeometry = new THREE.SphereGeometry(0.5);
 
-const boxMaterial = new THREE.MeshNormalMaterial();
+const boxMaterial = new THREE.MeshPhongMaterial({color: '#ff4fff',shininess: 55});
 const planeMaterial = new THREE.MeshBasicMaterial({ color: '#08531e' });
-const skyMaterial = new THREE.MeshBasicMaterial({ color: '#0c74d6' })
-const sunMaterial = new THREE.MeshBasicMaterial({ color: '#f6fa02'});
+const skyMaterial = new THREE.MeshBasicMaterial({ color: '#0c74d6' });
+const sunMaterial = new THREE.MeshBasicMaterial({ color: '#f6fa02' });
 
 const sun = new THREE.Mesh(sunGeometry, sunMaterial);
 const cube = new THREE.Mesh(boxGeometry, boxMaterial);
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 const sky = new THREE.Mesh(skyGeometry, skyMaterial);
 
-plane.position.y = -5
-cube.position.y = 0;
-sky.position.y = 15;
-sun.position.set(4, 13, 25);
-
-
+const sunLight = new THREE.PointLight('#ffffff', 2);
+const ambientLight = new THREE.AmbientLight('#ffffff',0.5);
 
 scene.add(sun);
 scene.add(cube);
 scene.add(plane);
 scene.add(sky);
+scene.add(sunLight);
+scene.add(ambientLight);
 
+plane.position.set(0, -5, 0);
+sky.position.set(0, 15, 0);
 
-
-
-
-
-
+sun.position.set(4, 13, 25);
+sunLight.position.set(4, 13, 25);
 
 
 function animate(time: number) {
-  cube.rotation.x = time / 20000;
-  cube.rotation.y = time / 10000;
-  controls.update();
+  cube.rotation.x = (time / 20000);
+  cube.rotation.y = (time / 10000);
+
   renderer.render(scene, camera);
 }
